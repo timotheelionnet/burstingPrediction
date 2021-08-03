5c60ada62f5b8cacad48d9a7f55b1efa95bd05a3
Mamrak <nm3037@nyumc.org>
2021-08-03 14:52:16 -0400

    Create findPredictors.m

---
+++ findPredictors.m
@@ -0,0 +1,19 @@
+function [topPredictors_activity, topPredictors_intensity] ...
+    = findPredictors(betaTrainAct, betaTrainInt, M, intList, A, I)
+summation = sum(M);
+
+betas_act = betaTrainAct;
+betas_act(1,:) = [];
+betas_act(:,2) = 1:numel(betas_ act);
+betas_act = sortrows(betas_act,'descend');
+actSum = find(summation>=A);
+topPredictors_activity = intList(actSum(betas_act(1:100,2)));
+
+betas_int = betaTrainInt;
+betas_int(1,:) = [];
+betas_int(:,2) = 1:numel(betas_int);
+betas_int = sortrows(betas_int,'descend');
+intSum = find(summation>=I);
+topPredictors_intensity = intList(intSum(betas_int(1:100,2)));
+
+end
