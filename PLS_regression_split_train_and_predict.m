39881e83a2fedf7fea98db622bc7fe174b9a9cdb
Mamrak <nm3037@nyumc.org>
2022-04-05 10:16:51 -0400

    Update PLS_regression_split_train_and_predict.m

--- PLS_regression_split_train_and_predict.m
+++ PLS_regression_split_train_and_predict.m
@@ -1,16 +1,17 @@
+figOpt = 0;
 tic
-itnum = 1000;
+itnum = 100;
 A=240;
 I=30;
 InteractionIndexAct = find(sum(M3)>=A);
 InteractionIndexInt = find(sum(M3)>=I);
 M3act = M3norm(:,InteractionIndexAct);
 M3int = M3norm(:,InteractionIndexInt);
-[m, n] = size(M3norm);
+%[m, n] = size(M3norm);
+%for q = 1:100
+%MscramAct = M3act(randperm(m),:);
+%MscramInt = M3int(randperm(m),:);
 trainingFraction = 10:10:80;
-for q = 1:100
-MscramAct = M3act(randperm(m),:);
-MscramInt = M3int(randperm(m),:);
 PLScorr_af = zeros(itnum,numel(trainingFraction));
 PLScorr_int = zeros(itnum,numel(trainingFraction));
 for i = 1:itnum
@@ -29,35 +30,41 @@ for i = 1:itnum
         W0int = statstrainint.W ./ sqrt(sum(statstrainint.W.^2,1));
         %% Predict AF scores for validation set
         predValidScoreAF = [ones(nValid,1), M3act(charTFind(validIndex),:)] * betaTrainAct;
-        %figure; scatter(burstingData2.activity(validIndex,1),predValidScoreAF); hold on;
-        %labelpoints(burstingData2.activity(validIndex,1),predValidScoreAF,charTFs(validIndex),'N',0.1)
-        %xlabel('True Activity Effect')
-        %ylabel('Predicted Activity Effect')
-        [ra,pa,rla,ra] = corrcoef(burstingData2.activity(validIndex),predValidScoreAF);
+        [ra,pa] = corr(burstingData2.activity(validIndex),predValidScoreAF,'type','Pearson');
         predValidTraincorrAct = ra;
-        %title(['Predicted vs True Values - Activity corr = ' num2str(predValidTraincorrAct)])
+
+        if figOpt == 1
+            figure; scatter(burstingData2.activity(validIndex,1),predValidScoreAF); hold on;
+            labelpoints(burstingData2.activity(validIndex,1),predValidScoreAF,burstingData2.TFnames(validIndex),'N',0.1)
+            xlabel('True Activity Effect')
+            ylabel('Predicted Activity Effect')
+            title(['Predicted vs True Values - Activity corr = ' num2str(predValidTraincorrAct)])
+        end
         %% Predict Int scores for validation set
         predValidScoreInt = [ones(nValid,1), M3int(charTFind(validIndex),:)] * betaTrainInt;
-        %figure; scatter(burstingData2.intensity(validIndex,1),predValidScoreInt); hold on;
-        %labelpoints(burstingData2.intensity(validIndex,1),predValidScoreInt,charTFs(validIndex),'N',0.1)
-        %xlabel('True Intensity Effect')
-        %ylabel('Predicted Intensity Effect')
-        [ri,pi,rli,ri] = corrcoef(burstingData2.intensity(validIndex),predValidScoreInt);
+        [ri,pi] = corr(burstingData2.intensity(validIndex),predValidScoreInt,"type","Pearson");
         predValidTraincorrInt = ri;
-        %title(['Predicted vs True Values - Intensity corr = ' num2str(predValidTraincorrInt)])
+
+        if figOpt == 1
+            figure; scatter(burstingData2.intensity(validIndex,1),predValidScoreInt); hold on;
+            labelpoints(burstingData2.intensity(validIndex,1),predValidScoreInt,burstingData2.TFnames(validIndex),'N',0.1)
+            xlabel('True Intensity Effect')
+            ylabel('Predicted Intensity Effect')
+            title(['Predicted vs True Values - Intensity corr = ' num2str(predValidTraincorrInt)])
+        end
         %%
-        %predTrainScoreact = [ones(nTraining,1), M3act(charTFind,InteractionIndexAct)] * betaTrainAct;
-        %predTrainTraincorrAct = corr(burstingData2.activity,predTrainScoreact);
-        %predTrainScoreint = [ones(nTraining,1), M3int(charTFind,InteractionIndexInt)] * betaTrainInt;
-        %predTrainTraincorrInt = corr(burstingData2.intensity,predTrainScoreint);
+        predTrainScoreact = [ones(nTraining,1), M3act(trainingIndex,:)] * betaTrainAct;
+        predTrainTraincorrAct = corr(burstingData2.activity(trainingIndex),predTrainScoreact);
+        predTrainScoreint = [ones(nTraining,1), M3int(trainingIndex,:)] * betaTrainInt;
+        predTrainTraincorrInt = corr(burstingData2.intensity(trainingIndex),predTrainScoreint);
         %%
-        PLScorr_af(i,j) = predValidTraincorrAct(1,2);
-        PLScorr_int(i,j) = predValidTraincorrInt(1,2);
+        PLScorr_af(i,j) = predValidTraincorrAct;
+        PLScorr_int(i,j) = predValidTraincorrInt;
 
     end
-end
-PLScorr_af(q,:) = mean(PLScorr_af);
-PLScorr_int(q,:) =  mean(PLScorr_int);
+    %end
+    %PLScorr_af(q,:) = mean(PLScorr_af);
+    %PLScorr_int(q,:) =  mean(PLScorr_int);
 end
 toc
 figure;
