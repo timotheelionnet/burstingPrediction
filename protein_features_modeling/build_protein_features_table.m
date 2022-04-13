8da97127a7afe3f2da65bb447380520568264e14
Mamrak <nm3037@nyumc.org>
2022-04-13 16:13:15 -0400

    Update build_protein_features_table.m
    added correction for multiple hypothesis testing

--- protein_features_modeling/build_protein_features_table.m
+++ protein_features_modeling/build_protein_features_table.m
@@ -1,12 +1,13 @@
+%% Run biogridScipt if needed
 if exist('RollingScores','var') == 0
     biogridScript; close all
 end
-%%
+%% Load LCR data
 allTFproteinIDFileName = 'allTF UniProtKB IDs.txt';
 allTFprotein_names  = readcell(allTFproteinIDFileName);
 lcr_table_FileName = 'LCR_table.txt';
 lcr_table  = readtable(lcr_table_FileName);
-%%
+%% Create LCR table for all TF list
 allTF_lcr_table(:,1) = table(tfList2);
 allTF_lcr_table(:,2:23) = {0};
 allTF_lcr_table.Properties.VariableNames = {'TFname','D','K','I','Y','G','R','M','E',...
@@ -18,17 +19,23 @@ for i = 1:numel(allTFprotein_names)
         allTF_lcr_table(i,23) = array2table(max(table2array(lcr_table(strmatch(allTFprotein_names(i), lcr_table.Var1),25))));
     end
 end
-%%
+%% Find signficant correlations between predicted effects and protein features with FDR correction
 rp_act = zeros(width(allTF_lcr_table),2)';
 rp_int = zeros(width(allTF_lcr_table),2)';
 for i = 2:width(allTF_lcr_table)
     [rp_act(1,i),rp_act(2,i)] = corr(RollingScores{5,2}(:,1),table2array(allTF_lcr_table(:,i)));
 end
+rp_act(3,:) = mafdr(rp_act(2,:));
+rp_act(4,:) = rp_act(2,:)<rp_act(3,:);
 for i = 2:width(allTF_lcr_table)
     [rp_int(1,i),rp_int(2,i)] = corr(RollingScores{5,2}(:,2),table2array(allTF_lcr_table(:,i)));
 end
-%%
+rp_int(3,:) = mafdr(rp_int(2,:));
+rp_int(4,:) = rp_int(2,:)<rp_int(3,:);
+%% Plot signficant correlations after FDR correction
+figure; scatter(table2array(allTF_lcr_table(:,3)),RollingScores{5,2}(:,1))
+ylim([0 3.5]); ylabel('Predicted Active Fraction Effect'); xlabel('Frequency of Lysine per TF');
+figure; scatter(table2array(allTF_lcr_table(:,22)),RollingScores{5,2}(:,1))
+ylim([0 3.5]); ylabel('Predicted Active Fraction Effect'); xlabel('Cumulative length of LCR segments per TF');
 figure; scatter(table2array(allTF_lcr_table(:,23)),RollingScores{5,2}(:,1))
 ylim([0 3.5]); ylabel('Predicted Active Fraction Effect'); xlabel('Number of LCR segments per TF');
-figure; scatter(table2array(allTF_lcr_table(:,23)),RollingScores{5,2}(:,2))
-ylim([0.4 2]); ylabel('Predicted Intensity Effect'); xlabel('Number of LCR segments per TF');
